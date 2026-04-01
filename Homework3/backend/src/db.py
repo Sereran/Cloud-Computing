4406121a0cb9c0d1aaac2ab9146b5499d063bfa5
Maria Ilade <mariailadem@gmail.com>
2026-04-01 23:13:20 +0300

    Cloud path fix

--- Homework3/backend/src/db.py
+++ Homework3/backend/src/db.py
@@ -15,7 +15,7 @@ def get_secret(secret_id: str, project_id: str = "937961278554") -> str:
     # Fetches a secret from Secret Manager
     try:
         client = secretmanager.SecretManagerServiceClient()
-        name = f"projects/{project_id}/secrets/{secret_id}/versions"
+        name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
         response = client.access_secret_version(request={"name": name})
         return response.payload.data.decode("UTF-8").strip()
     except Exception as e:
